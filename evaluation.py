773c3d6ec0080ece8c0bbf19d28e1b0b584f8d78
mlf20 <mlf20@imperial.ac.uk>
2022-01-22 21:59:16 +0000

    fix obs bug... ffs

--- evaluation.py
+++ evaluation.py
@@ -28,7 +28,7 @@ MAX_EPS = 10
 agent_name = 'Blue'
 
 def wrap(env):
-    return OpenAIGymWrapper(agent_name, 
+    return OpenAIGymWrapper(agent_name,
                             EnumActionWrapper(FixedFlatWrapper(ReduceActionSpaceWrapper(env))))
 
 
@@ -87,7 +87,7 @@ if __name__ == "__main__":
                 # cyborg.env.env.tracker.render()
                 for j in range(num_steps):
                     action = agent.get_action(observation, action_space)
-                    obs, rew, done, info = wrapped_cyborg.step(action)
+                    observation, rew, done, info = wrapped_cyborg.step(action)
 
                     # result = cyborg.step(agent_name, action)
                     r.append(rew)
