6efb526f706d5cf859576d69a5a52d5fa50af4ee
Vasilios Mavroudis <mavroudisv@gmail.com>
2022-01-24 19:52:16 +0000

    guild-fix + new dict for RED agents

--- agents_list.py
+++ agents_list.py
@@ -3,9 +3,18 @@ from agents.a2c.a2c.a2c_agent import A2CAgent
 # from agents.ppo.PPO import PPO as PPOAgent
 # from agents.dqn.dqn_agent import Agent as DQNAgent
 
-
 AGENTS = {#'rnd': BasicAgent, # Example, maps to BlueLoadAgent
             'a2c-rnd':A2CAgent, 
             'a2c':A2CAgent}#, 
             #'ppo':PPOAgent} #,
             # 'vas-ppo'=ppoAgent}
+
+
+from CybORG.Agents import B_lineAgent, SleepAgent
+from CybORG.Agents.SimpleAgents.Meander import RedMeanderAgent
+
+RED_AGENTS = {
+               'B_lineAgent': B_lineAgent,
+               'RedMeanderAgent': RedMeanderAgent,
+               'SleepAgent': SleepAgent,
+}
