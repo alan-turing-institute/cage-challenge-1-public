c4169d4a430fd41173f7adde933022e8ebde8fc0
mlf20 <mlf20@imperial.ac.uk>
2022-01-27 15:51:58 +0000

    hierarchical agent trained

---
+++ agents/hierachy_agents/hier_env.py
@@ -0,0 +1,162 @@
+import os
+import random
+import inspect
+import gym
+import ray
+import re
+from ray import tune
+
+from CybORG import CybORG
+from CybORG.Agents import B_lineAgent, GreenAgent, BaseAgent, RedMeanderAgent, BlueMonitorAgent
+from CybORG.Agents.Wrappers.BaseWrapper import BaseWrapper
+from CybORG.Agents.Wrappers import ChallengeWrapper
+from ray.rllib.env.env_context import EnvContext
+from gym import spaces
+from scaffold_env import CybORGScaffBL, CybORGScaffRM
+import ray.rllib.agents.ppo as ppo
+import numpy as np
+from ray.rllib.models import ModelCatalog
+from ray.rllib.models.torch.fcnet import FullyConnectedNetwork as TorchFC
+import torch
+from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
+
+
+class TorchModel(TorchModelV2, torch.nn.Module):
+    def __init__(self, obs_space, action_space, num_outputs, model_config,
+                 name):
+        TorchModelV2.__init__(self, obs_space, action_space, num_outputs, model_config,
+                 name)
+        torch.nn.Module.__init__(self)
+
+        self.model = TorchFC(obs_space, action_space,
+                                           num_outputs, model_config, name)
+
+    def forward(self, input_dict, state, seq_lens):
+        return self.model.forward(input_dict, state, seq_lens)
+
+    def value_function(self):
+        return self.model.value_function()
+
+
+class HierEnv(gym.Env):
+    path = str(inspect.getfile(CybORG))
+    path = path[:-10] + '/Shared/Scenarios/Scenario1b.yaml'
+
+    """The CybORGAgent env"""
+
+    def __init__(self, config: EnvContext):
+        agents = {
+            'Red': RedMeanderAgent  # B_lineAgent , #RedMeanderAgent, 'Green': GreenAgent
+        }
+
+        self.cyborg = CybORG(self.path, 'sim', agents={'Red':RedMeanderAgent})
+        self.RMenv  = ChallengeWrapper(env=self.cyborg, agent_name='Blue')
+        self.cyborg = CybORG(self.path, 'sim', agents={'Red':B_lineAgent})
+        self.BLenv  = ChallengeWrapper(env=self.cyborg, agent_name='Blue')
+
+        relative_path = os.path.abspath(os.getcwd())
+        self.BLcheckpoint_pointer = relative_path + '/log_dir/PPO_2022-01-26_13-44-08/PPO_CybORGScaff_09167_00000_0_2022-01-26_13-44-08/checkpoint_000400/checkpoint-400'
+        self.RMcheckpoint_pointer = relative_path + '/log_dir/PPO_2022-01-27_09-13-37/PPO_CybORGScaff_69289_00000_0_2022-01-27_09-13-38/checkpoint_000333/checkpoint-333'
+        sub_config = {
+            "env": CybORGScaffBL,
+            "env_config": {
+                "null": 0,
+            },
+            # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
+            "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
+            "model": {
+                "custom_model": "CybORG_PPO_Model",
+                "vf_share_layers": True,
+            },
+            "lr": 0.0001,
+            # "momentum": tune.uniform(0, 1),
+            "num_workers": 0,  # parallelism
+            "framework": "torch",  # May also use "tf2", "tfe" or "torch" if supported
+            "eager_tracing": True,  # In order to reach similar execution speed as with static-graph mode (tf default)
+            "vf_loss_coeff": 0.01,  # Scales down the value function loss for better comvergence with PPO
+            "in_evaluation": True,
+            'explore': False,
+            "exploration_config": {
+                "type": "Curiosity",  # <- Use the Curiosity module for exploring.
+                "eta": 1.0,  # Weight for intrinsic rewards before being added to extrinsic ones.
+                "lr": 0.001,  # Learning rate of the curiosity (ICM) module.
+                "feature_dim": 288,  # Dimensionality of the generated feature vectors.
+                # Setup of the feature net (used to encode observations into feature (latent) vectors).
+                "feature_net_config": {
+                    "fcnet_hiddens": [],
+                    "fcnet_activation": "relu",
+                },
+                "inverse_net_hiddens": [256],  # Hidden layers of the "inverse" model.
+                "inverse_net_activation": "relu",  # Activation of the "inverse" model.
+                "forward_net_hiddens": [256],  # Hidden layers of the "forward" model.
+                "forward_net_activation": "relu",  # Activation of the "forward" model.
+                "beta": 0.2,  # Weight for the "forward" loss (beta) over the "inverse" loss (1.0 - beta).
+                # Specify, which exploration sub-type to use (usually, the algo's "default"
+                # exploration, e.g. EpsilonGreedy for DQN, StochasticSampling for PG/SAC).
+                "sub_exploration": {
+                    "type": "StochasticSampling",
+                }
+            }
+        }
+        ModelCatalog.register_custom_model("CybORG_PPO_Model", TorchModel)
+
+        # Restore the checkpointed model
+        self.RM_def = ppo.PPOTrainer(config=sub_config, env=CybORGScaffBL)
+        sub_config['env'] = CybORGScaffRM
+        self.BL_def = ppo.PPOTrainer(config=sub_config, env=CybORGScaffRM)
+        self.BL_def.restore(self.BLcheckpoint_pointer)
+        self.RM_def.restore(self.RMcheckpoint_pointer)
+
+        self.steps = 1
+        self.agent_name = 'BlueHier'
+
+        #action space is 2 for each trained agent to select from
+        self.action_space = spaces.Discrete(2)
+
+        # observations for controller is a sliding window of 4 observations
+        self.observation_space = spaces.Box(-1.0,1.0,(52*4,), dtype=float)
+
+        #defuault observation is 4 lots of nothing
+        self.observation = np.zeros((52*4))
+
+        self.action = None
+        self.env = self.BLenv
+
+    # reset doesnt reset the sliding window of the agent so it can differentiate between
+    # agents across episode boundaries
+    def reset(self):
+        self.steps = 1
+        #rest the environments of each attacker
+        self.BLenv.reset()
+        self.RMenv.reset()
+        if random.choice([0,1]) == 0:
+            self.env = self.BLenv
+        else:
+            self.env = self.RMenv
+        return np.zeros((52*4))
+
+    def step(self, action=None):
+        # select agent
+        if action == 0:
+            # get action from agent trained against the B_lineAgent
+            agent_action = self.BL_def.compute_single_action(self.observation[-52:])
+        elif action == 1:
+            # get action from agent trained against the RedMeanderAgent
+            agent_action = self.RM_def.compute_single_action(self.observation[-52:])
+        else:
+            print('something went terribly wrong, old sport')
+        observation, reward, done, info = self.env.step(agent_action)
+
+        # update sliding window
+        observation = np.append(self.observation[52:], observation)
+        self.observation = observation
+
+        self.steps += 1
+        if self.steps == 100:
+            return observation, reward, True, info
+        assert(self.steps <= 100)
+        result = observation, reward, done, info
+        return result
+
+    def seed(self, seed=None):
+        random.seed(seed)
---
+++ agents/hierachy_agents/loadagent.py
@@ -0,0 +1,108 @@
+import os
+from pprint import pprint
+
+import numpy as np
+import ray
+from ray.rllib.agents.dqn.apex import APEX_DEFAULT_CONFIG
+from ray.rllib.agents.trainer import Trainer
+from ray.rllib.models import ModelCatalog
+from ray.rllib.env.env_context import EnvContext
+import ray.rllib.agents.ppo as ppo
+import ray.rllib.agents.dqn as dqn
+
+from CybORG import CybORG
+from CybORG.Agents.Wrappers.TrueTableWrapper import true_obs_to_table
+
+from train_hier import CustomModel, TorchModel
+from scaffold_env import CybORGScaffRM, CybORGScaffBL
+from hier_env import HierEnv
+import os
+class LoadBlueAgent:
+
+    """
+    Load the agent model using the latest checkpoint and return it for evaluation
+    """
+    def __init__(self) -> None:
+        ModelCatalog.register_custom_model("CybORG_PPO_Model", TorchModel)
+        relative_path = os.path.abspath(os.getcwd())
+        print(relative_path)
+        # load checkpoint locations of ech agent
+        self.checkpoint = relative_path + '/log_dir/PPO_2022-01-27_13-43-33/PPO_HierEnv_1e996_00000_0_2022-01-27_13-43-33/checkpoint_000212/checkpoint-212'
+        self.BLcheckpoint_pointer = relative_path + '/log_dir/PPO_2022-01-26_13-44-08/PPO_CybORGScaff_09167_00000_0_2022-01-26_13-44-08/checkpoint_000400/checkpoint-400'
+        self.RMcheckpoint_pointer = relative_path + '/log_dir/PPO_2022-01-27_09-13-37/PPO_CybORGScaff_69289_00000_0_2022-01-27_09-13-38/checkpoint_000333/checkpoint-333'
+
+        #with open ("checkpoint_pointer.txt", "r") as chkpopfile:
+        #    self.checkpoint_pointer = chkpopfile.readlines()[0]
+        print("Using checkpoint file: {}".format(self.checkpoint))
+        config = {
+            "env": HierEnv,
+            "env_config": {
+                "null": 0,
+            },
+            # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
+            "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
+            "model": {
+                "custom_model": "CybORG_PPO_Model",
+                "vf_share_layers": True,
+            },
+            "lr": 0.0001,
+            # "momentum": tune.uniform(0, 1),
+            "num_workers": 0,  # parallelism
+            "framework": "torch",  # May also use "tf2", "tfe" or "torch" if supported
+            "eager_tracing": True,  # In order to reach similar execution speed as with static-graph mode (tf default)
+            "vf_loss_coeff": 0.01,  # Scales down the value function loss for better comvergence with PPO
+            "in_evaluation": True,
+            'explore': False,
+        }
+
+        # Restore the controller model
+        self.controller_agent = ppo.PPOTrainer(config=config, env=HierEnv)
+        self.controller_agent.restore(self.checkpoint)
+        self.observation = np.zeros((52*4))
+
+        config["exploration_config"] ={
+                "type": "Curiosity",  # <- Use the Curiosity module for exploring.
+                "eta": 1.0,  # Weight for intrinsic rewards before being added to extrinsic ones.
+                "lr": 0.001,  # Learning rate of the curiosity (ICM) module.
+                "feature_dim": 288,  # Dimensionality of the generated feature vectors.
+                # Setup of the feature net (used to encode observations into feature (latent) vectors).
+                "feature_net_config": {
+                    "fcnet_hiddens": [],
+                    "fcnet_activation": "relu",
+                },
+                "inverse_net_hiddens": [256],  # Hidden layers of the "inverse" model.
+                "inverse_net_activation": "relu",  # Activation of the "inverse" model.
+                "forward_net_hiddens": [256],  # Hidden layers of the "forward" model.
+                "forward_net_activation": "relu",  # Activation of the "forward" model.
+                "beta": 0.2,  # Weight for the "forward" loss (beta) over the "inverse" loss (1.0 - beta).
+                # Specify, which exploration sub-type to use (usually, the algo's "default"
+                # exploration, e.g. EpsilonGreedy for DQN, StochasticSampling for PG/SAC).
+                "sub_exploration": {
+                    "type": "StochasticSampling",
+                }
+            }
+        #load agent trained against RedMeanderAgent
+        config['env'] = CybORGScaffRM
+        self.RM_def = ppo.PPOTrainer(config=config, env=CybORGScaffRM)
+        self.RM_def.restore(self.RMcheckpoint_pointer)
+        #load agent trained against B_lineAgent
+        config['env'] = CybORGScaffBL
+        self.BL_def = ppo.PPOTrainer(config=config, env=CybORGScaffBL)
+        self.BL_def.restore(self.BLcheckpoint_pointer)
+
+
+
+    """Compensate for the different method name"""
+    def get_action(self, obs, action_space):
+        #update sliding window
+        obs = np.append(self.observation[52:], obs)
+        self.observation = obs
+        #select agent to compute action
+        agent_to_select = self.controller_agent.compute_single_action(obs)
+        if agent_to_select == 0:
+            # get action from agent trained against the B_lineAgent
+            agent_action = self.BL_def.compute_single_action(self.observation[-52:])
+        elif agent_to_select == 1:
+            # get action from agent trained against the RedMeanderAgent
+            agent_action = self.RM_def.compute_single_action(self.observation[-52:])
+        return agent_action
